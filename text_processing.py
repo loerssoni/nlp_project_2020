c279b68bbe43a4df3f607ead5bcd0abeebbe681a
Lauri Heikka <lauri.heikka@gmail.com>
2020-10-10 10:39:09 +0300

    some optimisations and database initialization

--- text_processing.py
+++ text_processing.py
@@ -14,6 +14,7 @@ def get_category(label, categories, harv_inquirer, not_categories = []):
     words_t['label'] = label
     words_t.columns = words_t.columns.str.lower()
     words_t.drop_duplicates(inplace=True)
+
     return words_t
 
 def get_all_cat(category_dict, harv_inquirer):
@@ -21,14 +22,12 @@ def get_all_cat(category_dict, harv_inquirer):
     for label, categories in category_dict.items():
         df = df.append(get_category(label, categories['include'],
 			harv_inquirer, categories['exclude']))
+
     return df.reset_index(drop = True)
 
 def process_lexicon(texts):
     lexicon = Empath()
     data = {}
-
-    for i, text in texts.iteritems():
-        data[i] = [k for k, v in lexicon.analyze(text, normalize=False).items() if v > 0]
-
-        print("{:<5}%".format(round(i * 100 / len(texts), 2)), end='\r')
+    data = texts.apply(lambda x: [k for k, v in lexicon.analyze(x, normalize=False).items() if v > 0])
+    data = data.apply(pd.Series).stack().reset_index(level=1, drop=True)
     return data
